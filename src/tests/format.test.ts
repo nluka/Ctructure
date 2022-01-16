c7f9bf2a1307b9728662cbf55ba38801ed67a574
Nicholas Lukasevich <n.luka784@gmail.com>
2022-01-15 22:09:49 -0500

    Refactor format testing

--- src/tests/format.test.ts
+++ src/tests/format.test.ts
@@ -1,6 +1,27 @@
-import assert from "./assert";
+import { readFileSync } from 'fs';
+import debugLogFormatResult from '../debugLogFormatResult';
+import format from '../format';
+import removeCarriageReturns from '../utility/removeCarriageReturns';
+import path = require('path');
 
-assert('./asterisks/asterisks.c');
-assert('./empty_header/empty_header.h');
-assert('./ant_simulator_lite/command_line/command_line.c');
-assert('./ant_simulator_lite/timespan/timespan.c');
+describe('format', () => {
+  function assert(
+    filePathname: string,
+    shouldConsoleLogFormatResult = false,
+  ) {
+    test(filePathname, () => {
+      const resolvedFilePathname = path.resolve(__dirname, filePathname);
+      const fileContents = removeCarriageReturns(readFileSync(resolvedFilePathname).toString());
+      const formatResult = format(resolvedFilePathname);
+      if (shouldConsoleLogFormatResult) {
+        debugLogFormatResult(formatResult);
+      }
+      expect(formatResult).toBe(fileContents);
+    });
+  }
+
+  assert('./asterisks/asterisks.c');
+  assert('./empty_header/empty_header.h');
+  assert('./ant_simulator_lite/command_line/command_line.c');
+  assert('./ant_simulator_lite/timespan/timespan.c');
+});
