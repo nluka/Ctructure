1027db32aea62ffb7944962563be19e9f0356313
Nicholas Lukasevich <n.luka784@gmail.com>
2022-02-06 00:25:54 -0500

    Delete noFormat.c

--- src/tests/noFormat.c
+++
@@ -1,9 +0,0 @@
-// @Ctructure no format
-#include    <stdio.h>
-
-// @Ctructure no format
-int main(int   argc , char * *argv)  {
-  printf("Hello world!");
-  /* @Ctructure no format */
-return 0;
- }
