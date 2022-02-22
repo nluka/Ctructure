65f068c865fa0f672c938da5ebafcf9b8d2c97b4
Nicholas Lukasevich <n.luka784@gmail.com>
2022-02-22 16:27:30 -0500

    Begin DOOM format test suite: add am_map.c
    Use `npm run test formatDoom` to run this test suite

---
+++ src/tests/format_doom/formatDoom.test.ts
@@ -0,0 +1,8 @@
+import assert from "../assert";
+
+describe('formatDoom', () => {
+  const _assert = (filePathname: string, expected?: string): void =>
+    assert(`./format_doom/${filePathname}`, expected);
+
+  _assert('am_map.c');
+});
