27ac6a0b2e4eb65d9768989fd7b76b19a7ae3bef
Jallard <justinallard2001@gmail.com>
2023-06-30 20:22:25 -0400

    Change file extensions

--- src/tests/format_basic/formatBasic.test.ts
+++ src/tests/format_basic/formatBasic.test.ts
@@ -7,45 +7,44 @@ describe('formatBasic', () => {
     assert(`./format_basic/${filePathname}`, expected);
 
   {
-    _assert('ant_simulator_lite/ant.c');
-    _assert('ant_simulator_lite/command_line.c');
-    _assert('ant_simulator_lite/print.c');
+    _assert('ant_simulator_lite/ant.txt');
+    _assert('ant_simulator_lite/command_line.txt');
+    _assert('ant_simulator_lite/print.txt');
     _assert('ant_simulator_lite/print.h');
-    _assert('ant_simulator_lite/Timespan.c');
+    _assert('ant_simulator_lite/Timespan.txt');
   }
 
   {
     const helloWorldFormatted =
-      extractAndCleanFileContents(path.resolve(__dirname, './hello_world/hello_world_formatted.c'));
-    _assert('hello_world/hello_world_minified.c', helloWorldFormatted);
-    _assert('hello_world/hello_world_expanded.c', helloWorldFormatted);
+      extractAndCleanFileContents(path.resolve(__dirname, './hello_world/hello_world_formatted.txt'));
+    _assert('hello_world/hello_world_minified.txt', helloWorldFormatted);
+    _assert('hello_world/hello_world_expanded.txt', helloWorldFormatted);
   }
 
   {
-    _assert('string/String.c');
+    _assert('string/String.txt');
     _assert('string/String.h');
   }
 
   {
     const cleanedTokens =
-      extractAndCleanFileContents(path.resolve(__dirname, './token_cleaning/cleaned_tokens.c'));
-    _assert('token_cleaning/uncleaned_tokens.c', cleanedTokens);
+      extractAndCleanFileContents(path.resolve(__dirname, './token_cleaning/cleaned_tokens.txt'));
+    _assert('token_cleaning/uncleaned_tokens.txt', cleanedTokens);
   }
 
-  _assert('__attribute__.c');
-  _assert('asterisk.c');
-  _assert('binb.c');
-  _assert('comment.c');
+  _assert('__attribute__.txt');
+  _assert('asterisk.txt');
+  _assert('comment.txt');
   _assert('empty_header.h');
-  _assert('for.c');
-  _assert('hellish.c');
-  _assert('if_else.c');
-  _assert('label.c');
-  _assert('linux_macro_comment.c');
-  _assert('misc.c');
-  _assert('multivar_decl.c');
-  _assert('no_format.c');
-  _assert('prepro.c');
-  _assert('struct.c');
-  _assert('switch.c');
+  _assert('for.txt');
+  _assert('hellish.txt');
+  _assert('if_else.txt');
+  _assert('label.txt');
+  _assert('linux_macro_comment.txt');
+  _assert('misc.txt');
+  _assert('multivar_decl.txt');
+  _assert('no_format.txt');
+  _assert('prepro.txt');
+  _assert('struct.txt');
+  _assert('switch.txt');
 });
--- src/tests/format_doom/formatDoom.test.ts
+++ src/tests/format_doom/formatDoom.test.ts
@@ -4,66 +4,44 @@ describe('formatDoom', () => {
   const _assert = (filePathname: string, expected?: string): void =>
     assert(`./format_doom/${filePathname}`, expected);
 
-  _assert('am_map.c');
-  // d_englsh.h skipped
-  _assert('d_event.h');
-  // d_french.h skipped
-  _assert('d_items.c');
-  _assert('d_items.h');
-  _assert('d_main.c');
-  _assert('d_main.h');
-  _assert('d_net.c');
-  _assert('d_net.h');
-  _assert('d_player.h');
-  _assert('d_textur.h');
-  _assert('d_think.h');
-  _assert('d_ticcmd.h');
-  _assert('doomdata.h');
-  // doomdef.c skipped
-  _assert('doomdef.h');
-  _assert('doomstat.c');
-  _assert('doomstat.h');
-  // dstrings.c skipped
-  // dstrings.h skipped
-  _assert('f_finale.c');
-  // f_finale.h skipped
-  _assert('f_wipe.c');
-  _assert('f_wipe.h');
-  _assert('g_game.c');
-  // g_game.h skipped
-  _assert('hu_lib.c');
-  _assert('hu_lib.h');
-  _assert('hu_stuff.c');
-  // hu_stuff.h skipped
-  // i_main.c skipped
-  _assert('i_net.c');
-  // i_net.h skipped
-  _assert('i_sound.c');
-  // i_sound.h skipped
-  _assert('i_system.c');
-  // i_system.h skipped
-  _assert('i_video.c');
-  // i_video.h skipped
-  _assert('info.c');
-  _assert('info.h');
-  // m_argv.c skipped
-  // m_argv.h skipped
-  _assert('m_bbox.c');
-  // m_bbox.h skipped
-  _assert('m_cheat.c');
-  // m_cheat.h skipped
-  _assert('m_fixed.c');
-  // m_fixed.h skipped
-  _assert('m_menu.c');
-  // m_menu.h skipped
-  _assert('m_misc.c');
-  // m_misc.h skipped
-  _assert('m_random.c');
-  // m_random.h skipped
-  _assert('m_swap.c');
-  // m_swap.h skipped
-  _assert('p_ceilng.c');
-  _assert('p_doors.c');
-  _assert('p_enemy.c');
-  _assert('p_floor.c');
+  _assert('am_map.txt');
+  _assert('d_event.txt');
+  _assert('d_items.txt');
+  _assert('d_items.txt');
+  _assert('d_main.txt');
+  _assert('d_main 2.txt');
+  _assert('d_net.txt');
+  _assert('d_net.txt');
+  _assert('d_player.txt');
+  _assert('d_textur.txt');
+  _assert('d_think.txt');
+  _assert('d_ticcmd.txt');
+  _assert('doomdata.txt');
+  _assert('doomdef.txt');
+  _assert('doomstat.txt');
+  _assert('doomstat 2.txt');
+  _assert('f_finale.txt');
+  _assert('f_wipe.txt');
+  _assert('f_wipe 2.txt');
+  _assert('g_game.txt');
+  _assert('hu_lib.txt');
+  _assert('hu_lib.txt');
+  _assert('hu_stuff.txt');
+  _assert('i_net.txt');
+  _assert('i_sound.txt');
+  _assert('i_system.txt');
+  _assert('i_video.txt');
+  _assert('info.txt');
+  _assert('info 2.txt');
+  _assert('m_bbox.txt');
+  _assert('m_cheat.txt');
+  _assert('m_fixed.txt');
+  _assert('m_menu.txt');
+  _assert('m_misc.txt');
+  _assert('m_random.txt');
+  _assert('m_swap.txt');
+  _assert('p_ceilng.txt');
+  _assert('p_doors.txt');
+  _assert('p_enemy.txt');
+  _assert('p_floor.txt');
 });
